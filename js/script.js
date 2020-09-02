72401c1d33cf14095433d634eacbe9138f8dc06f
DARKEXCITE <63973918+DARKEXCITE@users.noreply.github.com>
2020-09-02 23:43:59 +0300

    v.3.1
    Добавлен каталог и добавлена шапка при скролле

--- js/script.js
+++ js/script.js
@@ -165,3 +165,39 @@ $('#headerSliderNext').click(function() {
 $('#headerSliderPrev').click(function() {
     headerCarousel.trigger('prev.owl.carousel');
 })
+// Выезжающее меню в каталоге
+const headerCatalogBtn = document.querySelector('.header__catalog-button')
+const headerCatalogMenu = document.querySelector('.header__catalog-menu')
+
+headerCatalogBtn.addEventListener('click', event => {
+    toggleCatalogMenu()
+    document.addEventListener('click', closeCatalogMenu)
+})
+
+const toggleCatalogMenu = () => {
+    headerCatalogMenu.classList.toggle('open')
+}
+
+const closeCatalogMenu = event => {
+    const target = event.target
+    if (!target.closest('.header__catalog-button') && !target.closest('.header__catalog-menu')) {
+        if (headerCatalogMenu.classList.contains('open')) {
+            if (target !== target.closest('.header__catalog-menu')) {
+                headerCatalogMenu.classList.toggle('open')
+            }
+        } else {
+            document.removeEventListener('click', closeCatalogMenu)
+        }
+    }
+}
+
+// Шапка при скролле
+const headerOnScroll = document.querySelector('.header-onscroll')
+
+window.onscroll = () => {
+    if (window.pageYOffset > 200) {
+        headerOnScroll.classList.remove('hide');
+    } else {
+        headerOnScroll.classList.add('hide');
+    }
+}
